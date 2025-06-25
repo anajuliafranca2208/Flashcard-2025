27fed5f74c420c7ae0c6f6c222acb17f8e0d8ee2
anajuliafranca2208 <ana.franca.goncalves@escola.pr.gov.br>
2025-06-25 16:01:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Quimica',
+    'O que é uma semi-reação?',
+    'É a parte de uma reação Redox, que mostra apenas a oxidação ou a redução'
+)
+
+criaCartao(
+    'Quimica',
+    'O que é uma reação espontânea?',
+    'É aquela que ocorre por si só, sem precisar de energia externa contínua para acontecer'
+)
+
+criaCartao(
+    'Quimica',
+    'O que é a Cinética Quimica?',
+    'É a área da quimica responsável pelo estudo da velocidade das reações quimicas'
+)
+
+criaCartao(
+    'Quimica',
+    '',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Quimica',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Quimica',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
